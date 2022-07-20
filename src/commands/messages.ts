52ec07a72dbec89e1123cbb75198482a07557a5d
Arjun Rasodha <arjunrasodha@Arjuns-MacBook-Pro.local>
2022-07-20 12:18:37 -0400

    formatted message command
    Co-authored-by: Tony Beere <tonybeere87@gmail.com>
    Co-authored-by: Anna Kolliakou <akolliakou@gmail.com>
    Co-authored-by: Kris Ingva <krisingva@gmail.com>

--- src/commands/messages.ts
+++ src/commands/messages.ts
@@ -1,9 +1,30 @@
 import {Command} from '@oclif/core'
 const axios = require('axios');
-export default class World extends Command {
+import { argv } from 'process';
+
+export default class Message extends Command {
+  static args = [
+    {name: 'id'}
+  ]
 
   async run(): Promise<void> {
     const response = await axios.get('http://localhost:3000/table/allMessages')
-    await this.log(response.data);
+    // const {args}: any = this.parse(Message);
+    // if (args.id) {
+    //   response.data = response.data.filter((msg: any) => msg.id === args.id)
+    //   if (response.data.length === 0) {
+    //     this.log('There are no message with that id.')
+    //     return
+    //   }
+    // }
+
+
+    response.data.forEach((msg:any, idx: number) => {
+      this.log(`Message #${idx + 1}:`)
+      this.log('Message ID: ', msg.id)
+      this.log('Message Body: ', msg.Message)
+      this.log('Message Attributes: ', JSON.parse(msg.Attributes))
+      this.log('-------------------------------------------------------\n')
+    })
   }
 }
